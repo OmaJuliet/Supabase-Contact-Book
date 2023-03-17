96ccf0c372d026f1b79c896b27177f9e26e1b522
Juliet Oma <julieto2001@gmail.com>
2023-03-17 12:30:54 +0100

    lazy loaded the web app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
-import Update from './Pages/Update';
-import Create from './Pages/Create';
+// import Home from './Pages/Home';
+// import Update from './Pages/Update';
+// import Create from './Pages/Create';
+import React, { lazy, Suspense } from 'react';
+import Loading from './Components/Loading';
+
+const Home = lazy(() => import("./Pages/Home"));
+const Create = lazy(() => import("./Pages/Create"));
+const Update = lazy(() => import("./Pages/Update"));
 
 
 function App() {
@@ -11,11 +17,14 @@ function App() {
     <>
       <Navbar />
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<Create />} />
-          <Route path="/:id" element={<Update />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/:id" element={<Update />} />
+          </Routes>
+        </Suspense>
+
       </Router>
     </>
   );
---
+++ src/Components/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <>
+            <h1 className="flex flex-col text-center font-medium mx-auto mt-2 lg:mt-2 text-3xl"> Loading.... </h1>
+        </>
+    )
+}
+
+export default Loading
